b99458fa95844455fe4c34960ae3170dfe52996f
DarwinSalcedo <iamdarwin.salcedo@gmail.com>
2026-01-01 20:52:11 -0300

    extracting FRONTEND_URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:3000/api', // Make this configurable via env vars ideally
+    baseURL: import.meta.env.FRONTEND_URL || 'http://localhost:3000/api',
     headers: {
         'Content-Type': 'application/json',
     },
